d2d2517282a1922d55b2020328fbdfbaed2cc744
Rodriguez Garcia Juan Pablo <112775638+JuanPablo2003@users.noreply.github.com>
2025-02-27 18:55:45 -0600

    Más progreso no funcional

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\PageController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', [PageController::class, 'about']);
