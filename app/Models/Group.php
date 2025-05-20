20c6b031333a344124400f47d8476df9e11e97b9
SantiagoEsc1216 <santiagoesc1216@gmail.com>
2025-05-20 13:15:11 -0600

    se agrego modelo y controlador de grupos

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -10,7 +10,13 @@ class Group extends Model
     protected $table = 'groups';
     use HasFactory;
     protected $fillable = [
-        'group_id'
+        'group_id',
+        'semester',
+        'type_a',
+        'period_a',
+        'subject_id',
+        'subject_id',
+        'hour_a'
     ];
     public $timestamps = false;
     protected $primaryKey = 'group_id';
