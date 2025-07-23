23d970868010903331a5baff5b56ef3175e45e28
santiagoesc1216 <santiagoesc1216@gmail.com>
2025-07-23 14:46:02 -0600

    modelos de permisos, roles y permisos de roles

---
+++ app/Models/RolePermission.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RolePermission extends Model
+{
+    use HasFactory;
+    protected $table = 'role_permissions';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'role_id',
+        'permission_id',
+        'is_enabled',
+    ];
+
+    // Relaciones
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    public function permission()
+    {
+        return $this->belongsTo(Permission::class, 'permission_id');
+    }
+}
