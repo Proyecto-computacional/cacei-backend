4475a3bd4136aa97c813cce9752d0b42fdcd63a9
BRYANAARONEDO2 <124536363+BRYANAARONEDO2@users.noreply.github.com>
2025-09-26 18:53:22 -0600

    Actualizando base de datos

--- database/sql/Cacei.sql
+++ database/sql/Cacei.sql
@@ -152,8 +152,8 @@ CREATE TABLE academic_managements (
     cv_id BIGINT,
     job_position VARCHAR(100),
     institution VARCHAR(50),
-    start_date DATE,
-    end_date DATE,
+    start_date VARCHAR(7),
+    end_date VARCHAR(7),
     PRIMARY KEY (academic_management_id),
     FOREIGN KEY (cv_id) REFERENCES cvs(cv_id)
 );
@@ -172,8 +172,8 @@ CREATE TABLE laboral_experiences (
     cv_id BIGINT,
     company_name VARCHAR(60),
     position VARCHAR(60),
-    start_date DATE,
-    end_date DATE,
+    start_date VARCHAR(7),
+    end_date VARCHAR(7),
     PRIMARY KEY (laboral_experience_id),
     FOREIGN KEY (cv_id) REFERENCES cvs(cv_id)
 );
@@ -201,7 +201,7 @@ CREATE TABLE participations (
     cv_id BIGINT,
     institution VARCHAR(30),
     period INT,
-    level_participation VARCHAR(20),
+    level_participation INT,
     PRIMARY KEY (participation_id),
     FOREIGN KEY (cv_id) REFERENCES cvs(cv_id)
 );
@@ -353,4 +353,3 @@ INSERT INTO careers (career_id, area_id, career_name) VALUES
 ('CA12', 'AR05', 'Ingeniería Mecatrónica'),
 ('CA13', 'AR06', 'Ingeniería Metalúrgica y de Materiales'),
 ('CA14', 'AR03', 'Ingeniería Geoinformática');
-
