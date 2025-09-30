39775554780ef2a425a6fd12b4c13cd3243bbc4d
Rodriguez Garcia Juan Pablo <112775638+JuanPablo2003@users.noreply.github.com>
2025-09-30 11:55:06 -0600

    Update GenerateNotifications.php

--- app/Console/Commands/GenerateNotifications.php
+++ app/Console/Commands/GenerateNotifications.php
@@ -17,39 +17,39 @@ class GenerateNotifications extends Command
     {
         //PRUEBA 
         // Simula la fecha de hoy como 2025-06-30
-   // $hoy = Carbon::create(2025, 6, 30)->format('Y-m-d'); 
+        // $hoy = Carbon::create(2025, 6, 30)->format('Y-m-d'); 
 
-    // Obtener evidencias con fecha de vencimiento igual a la fecha simulada
-    //$evidencias = Evidence::where('due_date', $hoy)->get();
+        // Obtener evidencias con fecha de vencimiento igual a la fecha simulada
+        //$evidencias = Evidence::where('due_date', $hoy)->get();
 
-        $hoy = Carbon::now(); 
-       $tresDiasAntes = $hoy->addDays(3)->format('Y-m-d');
+        $hoy = Carbon::now();
+        $tresDiasAntes = $hoy->addDays(3)->format('Y-m-d');
 
         // Obtener evidencias con fecha de vencimiento en 3 días
         $evidencias = Evidence::where('due_date', $tresDiasAntes)->get();
 
         foreach ($evidencias as $evidencia) {
             // Buscar un revisor que tenga asignada la evidencia
-              $revisor = Reviser::where('evidence_id', $evidencia->evidence_id)->first();
+            $revisor = Reviser::where('evidence_id', $evidencia->evidence_id)->first();
 
-             if (!$revisor) {
-                             $this->error("No hay revisor asignado para la evidencia ID: {$evidencia->evidence_id}");
-                                continue; // Saltar esta iteración si no hay revisor
-                             }
+            if (!$revisor) {
+                $this->error("No hay revisor asignado para la evidencia ID: {$evidencia->evidence_id}");
+                continue; // Saltar esta iteración si no hay revisor
+            }
             try {
-            Notification::create([
-                'title' => 'Vence evidencia',
-                'evidence_id' => $evidencia->evidence_id,
-                'notification_date' => Carbon::now(),
-                'user_rpe' => $evidencia->user_rpe, // Notificar al usuario responsable
-                'reviser_id' => $revisor->reviser_id,
-                'description' => 'vence en 3 días.',
-                'seen' => false,
-                'pinned' => false
-            ]);
-        } catch (\Exception $e) {
-            $this->error('Error al crear la notificación: ' . $e->getMessage());
-        }
+                Notification::create([
+                    'title' => 'Vence evidencia',
+                    'evidence_id' => $evidencia->evidence_id,
+                    'notification_date' => Carbon::now(),
+                    'user_rpe' => $evidencia->user_rpe, // Notificar al usuario responsable
+                    'reviser_id' => $revisor->reviser_id,
+                    'description' => 'vence en 3 días.',
+                    'seen' => false,
+                    'pinned' => false
+                ]);
+            } catch (\Exception $e) {
+                $this->error('Error al crear la notificación: ' . $e->getMessage());
+            }
         }
 
         $this->info('Notificaciones generadas correctamente.');
